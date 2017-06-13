0d0148b69c6b8c701abdc77439548febb050b35a
sup3rcow <bullandrooster@gmail.com>
2017-06-13 14:35:10 +0200

    implementacicja DAL.. URLdb.cs

---
+++ DAL/URLdb.cs
@@ -0,0 +1,45 @@
+﻿using BOL;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DAL
+{
+    class URLdb
+    {
+        private LinkHubDbEntities db;
+        public URLdb()
+        {
+            db = new LinkHubDbEntities();
+        }
+        public IEnumerable<tbl_Url> GetAll()
+        {
+            return db.tbl_Url.ToList();
+        }
+        public tbl_Url GetById(int id)
+        {
+            return db.tbl_Url.First(u => u.UrlId == id);//koristis First jer je brzi od single, jer se odmah zaustavi kad nadje prvog
+            //u primjeru koristi Find(id) - Find should be as fast as First, but is less portable as it will only work on lists. 
+        }
+        public void Insert(tbl_Url url)
+        {
+            db.tbl_Url.Add(url);
+            Save();
+        }
+        public void Delete(int id)
+        {
+            var url = db.tbl_Url.First(u => u.UrlId == id);
+            db.tbl_Url.Remove(url);
+        }
+        public void Update(tbl_Url url)
+        {
+            db.Entry(url).State = EntityState.Modified;
+            //moze i da ga nadjes u bazi, promijenis propertije i onda Save(), ali ovo je jednostavniej
+        }
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+    }
+}
